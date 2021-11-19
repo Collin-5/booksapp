72cf3a330c17cfa1cb268cb6e00cd1b2b8b175dc
Collin-5 <collins380@gmail.com>
2021-11-19 22:02:22 +0300

    statics

--- books/settings.py
+++ books/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party
+    'crispy_forms',
+
     # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
@@ -128,6 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -140,3 +149,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # redirection pages
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
+
+# Django crispy_forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
